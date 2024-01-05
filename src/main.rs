eb96f696cb2be7f43c2411dc8fee2989a8389c09
TSwaffer <tswaffer@riftgroup.com>
2024-01-05 10:47:44 +0000

    Integer types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
-    let mut x = 10;
+    let mut x: u8 = 10;
     println!("x is {}", x);
     x = 20;
     println!("x is {}", x);
 }
+
+// Integer types
+// Signed allows negative values
+// Unisgned are only positive values
+// Length	Signed	Unsigned
+// 8-bit	i8	    u8
+// 16-bit	i16	    u16
+// 32-bit	i32	    u32
+// 64-bit	i64	    u64
+// 128-bit	i128	u128
+// arch	    isize	usize
